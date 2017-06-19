cee508b66efe7e45453af2f2b3a40b3f7a292aee
Michaella <mtalento29@gmail.com>
2017-06-19 17:18:25 +0800

    Setup start sccript and Heroku port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require("hbs");
 
 const fs = require("fs");
 
+const port = process.env.PORT || 1000;
+
 var app = express();
 
 hbs.registerPartials(__dirname+"/views/partials");
@@ -108,6 +110,6 @@ app.get("/bad", (request, response) => {
 
 //listen arguments
 //listen (port number, a function)
-app.listen(1000, () =>{
-    console.log("Server is up and running on port 1000");
+app.listen(port, () =>{
+    console.log(`Server is up and running on port ${port}`);
 });
